a70bb2d2fe7842e8426060f88637364f6c9932ae
97wanner97 <cwa143811@stud.gibb.ch>
2024-03-28 11:07:47 +0100

    database: adds MongoDBSettings.cs file

---
+++ Common/Configurations/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Configurations
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
